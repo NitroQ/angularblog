fa8b84a54f6301b1f8b11ea4125a96a048b7b6a6
NitroQ <frankgesmundo10@gmail.com>
2022-10-14 19:04:48 +0800

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { LoginComponent } from './login/login.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {component:HomeComponent, path: ''},
+  {component:ContactComponent, path: 'contact'},
+  {component:GalleryComponent, path: 'gallery' },
+  {component:LoginComponent, path: 'login'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
